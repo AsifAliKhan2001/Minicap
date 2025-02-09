f6df96401dac2aebdbb0c91cbf2779a83e622abc
Hudson Lu <131294600+HudsonLu@users.noreply.github.com>
2025-02-08 23:00:41 -0500

    Component folder tests

--- src/__test__/components/ThemedText.test.tsx
+++ src/__test__/components/ThemedText.test.tsx
@@ -3,10 +3,11 @@ import renderer from "react-test-renderer";
 
 import { ThemedText } from "../../components/ThemedText";
 
-it(`renders correctly`, () => {
-  const tree = renderer
-    .create(<ThemedText>Snapshot test!</ThemedText>)
-    .toJSON();
-
-  expect(tree).toMatchSnapshot();
+describe("ThemedText Component", () => {
+  it("renders correctly", () => {
+    const tree = renderer
+      .create(<ThemedText>Snapshot test!</ThemedText>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
