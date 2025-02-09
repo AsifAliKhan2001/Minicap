27fb2f80b7ab02e90bcb0ace6d0969b74d3e30f7
Hudson Lu <131294600+HudsonLu@users.noreply.github.com>
2025-02-09 16:48:45 -0500

    Fix UT report

--- src/jest.config.js
+++ src/jest.config.js
@@ -1,13 +1,10 @@
 module.exports = {
   preset: "jest-expo",
   collectCoverage: true,
-  coverageDirectory: "coverage",
-  testPathIgnorePatterns: ["/node_modules/"],
-  coverageReporters: ["text", "lcov", "text-summary"],
-  transformIgnorePatterns: [
-    "node_modules/(?!react-native|react-navigation|@react-native|expo|@expo|@unimodules|@react-navigation|@react-native-community|@gluestack-ui)"
+  coverageDirectory: "src/coverage",
+  coverageReporters: ["text", "lcov", "text-summary", "clover", "json"],
+  reporters: [
+    "default",
+    ["jest-junit", { outputDirectory: "src/test-results", outputName: "junit.xml" }]
   ],
-  moduleNameMapper: {
-    "\\.(jpg|jpeg|png|gif|svg)$": "<rootDir>/__mocks__/fileMock.js"
-  },
 };
