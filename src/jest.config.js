d3c3d02de554d9d84444fcceeef6b15fd223ae79
Hudson Lu <131294600+HudsonLu@users.noreply.github.com>
2025-02-09 10:35:34 -0500

    CodeCov Report

--- src/jest.config.js
+++ src/jest.config.js
@@ -2,7 +2,8 @@ module.exports = {
     preset: "jest-expo",
     collectCoverage: true,
     coverageDirectory: "coverage",
-    coverageReporters: ["text", "lcov"],
+    testPathIgnorePatterns: ["/node_modules/"], // Make sure `app/` is not ignored
+    coverageReporters: ["text", "lcov", "text-summary"],
     reporters: [
       "default",
       ["jest-junit", { outputDirectory: "./test-results", outputName: "junit.xml" }]
