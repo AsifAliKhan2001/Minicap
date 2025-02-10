27f2836f371c71f2480fdd6ce88235743ee6829d
vibqetowi <abutcher.usa@protonmail.com>
2025-02-09 20:32:53 -0500

    Merge branch '107--mvp0---Database-Setup---Campus' of https://github.com/AsifAliKhan2001/Minicap into 88-mvp-0---basic-login-and-user-data-persistence

--- src/viewmodels/UserViewModel.ts
+++ src/viewmodels/UserViewModel.ts
@@ -5,49 +5,65 @@ import { UserRepository } from "@/repositories/UserRepository";
 import { Audit } from "@/models/Audit";
 
 export class UserViewModel extends BaseViewModel<User> implements UserRepository {
-    private readonly COLLECTION = "users";
+    private readonly COLLECTION = "Users_Info";
 
+    // These were the only methods implemented in server.js
+    async getAllUsers(): Promise<User[]> {
+        return await this.withCollection(this.COLLECTION, async (collection) => {
+            const users = await collection.find().toArray();
+            return users.map(user => this.mapToDTO(user));
+        });
+    }
+
+    async createUser(userData: Omit<User, "_id">): Promise<User> {
+        return await this.withCollection(this.COLLECTION, async (collection) => {
+            const result = await collection.insertOne(userData);
+            const newUser = await collection.findOne({ _id: result.insertedId });
+            return this.mapToDTO(newUser!);
+        });
+    }
+
+    // All other repository methods should throw errors as they weren't implemented in server.js
     async findUserById(id: ObjectId): Promise<User> {
-        throw new Error("Method not implemented: findUserById");
+        throw new Error("Method not implemented");
     }
 
     async signUp(data: Omit<User, "id" | "createdAt" | "updatedAt">): Promise<{ user: User; token: string }> {
-        throw new Error("Method not implemented: signUp");
+        throw new Error("Method not implemented");
     }
 
     async login(email: string, password: string): Promise<{ user: User; token: string }> {
-        throw new Error("Method not implemented: login");
+        throw new Error("Method not implemented");
     }
 
     async logout(token: string): Promise<void> {
-        throw new Error("Method not implemented: logout");
+        throw new Error("Method not implemented");
     }
 
     async updateUser(id: ObjectId, data: Partial<User>, token: string): Promise<User> {
-        throw new Error("Method not implemented: updateUser");
+        throw new Error("Method not implemented");
     }
 
     async deleteUser(id: ObjectId, token: string): Promise<void> {
-        throw new Error("Method not implemented: deleteUser");
+        throw new Error("Method not implemented");
     }
 
     async findByEmail(email: string): Promise<User | null> {
-        throw new Error("Method not implemented: findByEmail");
+        throw new Error("Method not implemented");
     }
 
     protected mapToDTO(doc: any): User {
         if (!doc) throw new Error('Document not found');
         return {
             _id: doc._id,
-            ...doc,
-            createdAtUTC: doc.createdAtUTC,
-            updatedAtUTC: doc.updatedAtUTC,
-            createdBy: doc.createdBy,
-            updatedBy: doc.updatedBy
-        } as User;
+            name: doc.name,
+            email: doc.email,
+            password: doc.password,
+            calendarIds: doc.calendarIds || []
+        };
     }
 
     protected async updateAudit(existingAudit: Partial<Audit> | null, userId: ObjectId): Promise<Audit> {
-        throw new Error("Method not implemented: updateAudit");
+        throw new Error("Method not implemented");
     }
 }
